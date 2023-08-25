7fb92634c4f7285497f03c1df6ead00d6177cc06
Vicente2010 <a01688409@tec.mx>
2023-08-24 20:42:10 -0600

    modify logging_util.py to add new method for StreamHandler

--- utilities/logging_util.py
+++ utilities/logging_util.py
@@ -44,3 +44,38 @@ class LoggingSetter:
         logger.addHandler(file_handler)
 
         return logger
+
+    def setting_log_stream(self, log_name):
+        """
+        Sets the configuration for a log
+
+        Returns:
+            str: The logger
+        """
+
+        # Extract the first cabin | Extract the title from 'name'
+
+        # Taking modudle's name
+        logger = logging.getLogger(self.name)
+
+        # Setting loggin level
+        logger.setLevel(logging.DEBUG)
+
+        # Setting logging format
+        formatter = logging.Formatter('%(asctime)s:%(name)s:%(module)s:%(levelname)s:%(message)s')
+
+        # Setting logging name
+        file_handler = logging.FileHandler(log_name)
+
+        # Configuring format in the file handler
+        file_handler.setFormatter(formatter)
+
+        # Adding the log file
+        logger.addHandler(file_handler)
+
+        # Adding the stream Handler
+        stream_handler = logging.StreamHandler()
+        stream_handler.setFormatter(formatter)
+        logger.addHandler(stream_handler)
+
+        return logger
