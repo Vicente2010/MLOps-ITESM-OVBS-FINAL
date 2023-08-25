b3792aa850a06f50db5149668377712b24e7811d
Vicente2010 <a01688409@tec.mx>
2023-08-24 20:49:57 -0600

    Add frontend_main.py for generating an api that consumes the predition Api

---
+++ frontend/frontend_main.py
@@ -0,0 +1,58 @@
+import requests
+from fastapi import FastAPI, Body
+
+# This module will not use the logging utility since is a level bellow to use it
+import logging
+
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
+
+formatter = logging.Formatter("%(levelname)s: %(asctime)s|%(name)s|%(message)s")
+
+file_handler = logging.FileHandler("frontend.log")
+file_handler.setFormatter(formatter)
+
+stream_handler = logging.StreamHandler()
+stream_handler.setFormatter(formatter)
+
+logger.addHandler(file_handler)
+logger.addHandler(stream_handler)
+
+# Starting API
+app = FastAPI()
+
+
+# ML model prediction function using the prediction API request
+def predict_online_fraud(input):
+    url3 = "http://app.docker:8000/predict_fraud_dt"
+
+    response = requests.post(url3, json=input)
+    response = response.text
+
+    return response
+
+
+@app.get("/")
+def read_root():
+    logger.info("Front-end is all ready to go!")
+    return "Front-end is all ready to go!"
+
+
+@app.post("/classify")
+def classify(payload: dict = Body(...)):
+    logger.debug(f"Incoming input in the front end: {payload}")
+    response = predict_online_fraud(payload)
+    logger.debug(f"Prediction: {response}")
+    return {"response": response}
+
+
+@app.get("/healthcheck")
+async def v1_healhcheck():
+    url3 = "http://app.docker:8000/"
+
+    response = requests.request("GET", url3)
+    response = response.text
+    logger.info(f"Checking health: {response}")
+
+    return response
